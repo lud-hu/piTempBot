35175459b7fe3a1852a730f3163f4474ab8c619a
Ludwig <ludwig.hubert@gmail.com>
2018-02-12 23:34:33 +0000

    imported missing json module

--- netatmo.py
+++ netatmo.py
@@ -1,5 +1,6 @@
 from netatmo_client.client import NetatmoClient
 import config as cfg
+import json
 
 scopes = ('read_station', 'read_thermostat', 'write_thermostat', 'read_camera')
 
@@ -8,5 +9,5 @@ client = NetatmoClient(cfg.credentials["client_id"], cfg.credentials["client_sec
 client.request_token_with_client_credentials(cfg.credentials["username"], cfg.credentials["password"], *scopes)
 
 # currently just receiving the JSON, not doing anything with it
-result = client.public.get_station_data(cfg.region["lat_ne"], cfg.region["lon_ne"], cfg.region["lat_sw"], cfg.region["lon_sw"])
+result = client.public.get_public_data(cfg.region["lat_ne"], cfg.region["lon_ne"], cfg.region["lat_sw"], cfg.region["lon_sw"])
 print json.dumps(result)
