73f103c79b7549a330e9acb9fe5dd586b4f187ed
Ludwig <ludwig.hubert@gmail.com>
2018-02-13 00:25:58 +0100

    fixed typos and dict lookup

--- netatmo.py
+++ netatmo.py
@@ -3,10 +3,10 @@ import config as cfg
 
 scopes = ('read_station', 'read_thermostat', 'write_thermostat', 'read_camera')
 
-client = NetatmoClient(cfg.credentials.client_id, cfg.credentials.client_secret)
+client = NetatmoClient(cfg.credentials[client_id], cfg.credentials[client_secret])
 
-client.request_token_with_client_credentials(cfg.credentials.username, cfg.credentials.password, *scopes)
+client.request_token_with_client_credentials(cfg.credentials.[username], cfg.credentials[password], *scopes)
 
 # currently just receiving the JSON, not doing anything with it
-var result = client.public.get_station_data(cfg.region.lat_ne, cfg.region.lon_ne, cfg.region.lat_sw, cfg.region.lon_sw)
+var result = client.public.get_station_data(cfg.region[lat_ne], cfg.region[lon_ne], cfg.region[lat_sw], cfg.region[lon_sw])
 print json.dumps(result)
