1a4d7de96305ed7cbc6f5812af60b99af31944a1
Ludwig <ludwig.hubert@gmail.com>
2018-02-13 00:28:52 +0100

    and another typo again

--- netatmo.py
+++ netatmo.py
@@ -5,7 +5,7 @@ scopes = ('read_station', 'read_thermostat', 'write_thermostat', 'read_camera')
 
 client = NetatmoClient(cfg.credentials[client_id], cfg.credentials[client_secret])
 
-client.request_token_with_client_credentials(cfg.credentials.[username], cfg.credentials[password], *scopes)
+client.request_token_with_client_credentials(cfg.credentials[username], cfg.credentials[password], *scopes)
 
 # currently just receiving the JSON, not doing anything with it
 result = client.public.get_station_data(cfg.region[lat_ne], cfg.region[lon_ne], cfg.region[lat_sw], cfg.region[lon_sw])
